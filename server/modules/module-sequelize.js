30aaa8c4fc1913fc58e6243093bb3fd9ecceec13
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-11-03 08:37:15 -0500

    Cleaning up database interaction.

--- server/modules/module-sequelize.js
+++ server/modules/module-sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const config = require('../../config')
-sequelize = new Sequelize({
+const config = require('../../config');
+const sequelize = new Sequelize({
     database: config.DB_NAME,
     username: config.DB_USERNAME,
     password: config.DB_PASSWORD,
